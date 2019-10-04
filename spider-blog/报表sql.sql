55a507c5dae12dccec2ddc8c830536198e86e375
inwsy <inwsy@hotmail.com>
2019-10-04 12:03:19 +0800

    更新 sql 脚本，增加 readme 文件

--- spider-blog/报表sql.sql
+++ spider-blog/报表sql.sql
@@ -1,13 +1,31 @@
 -- 因为掘金使用的是排名是掘力值，顾取出来的数据是负数
-SELECT
-	a.read_num - (SELECT b.read_num FROM spider_data b WHERE b.plantform = 'cnblog' AND DATE_FORMAT(b.create_date, '%Y-%m-%d') = date_sub(DATE_FORMAT(a.create_date, '%Y-%m-%d'), interval 1 day)) AS read_num,
-	a.fans_num - (SELECT b.fans_num FROM spider_data b WHERE b.plantform = 'cnblog' AND DATE_FORMAT(b.create_date, '%Y-%m-%d') = date_sub(DATE_FORMAT(a.create_date, '%Y-%m-%d'), interval 1 day)) AS fans_num,
-	a.like_num - (SELECT b.like_num FROM spider_data b WHERE b.plantform = 'cnblog' AND DATE_FORMAT(b.create_date, '%Y-%m-%d') = date_sub(DATE_FORMAT(a.create_date, '%Y-%m-%d'), interval 1 day)) AS like_num,
-	(SELECT b.rank_num FROM spider_data b WHERE b.plantform = 'cnblog' AND DATE_FORMAT(b.create_date, '%Y-%m-%d') = date_sub(DATE_FORMAT(a.create_date, '%Y-%m-%d'), interval 1 day)) - a.rank_num AS rank_num,
-	a.create_date
-FROM
-	spider_data a
-WHERE
-	a.plantform = 'cnblog' 
-GROUP BY
-	DATE_FORMAT(a.create_date, '%Y-%m-%d');
+SELECT a.read_num - (
+		SELECT b.read_num
+		FROM spider_data b
+		WHERE b.plantform = a.plantform
+			AND DATE_FORMAT(b.create_date, '%Y-%m-%d') = date_sub(DATE_FORMAT(a.create_date, '%Y-%m-%d'), INTERVAL 1 DAY) 
+		ORDER BY b.create_date DESC LIMIT 1
+	) AS read_num, a.fans_num - (
+		SELECT b.fans_num
+		FROM spider_data b
+		WHERE b.plantform = a.plantform
+			AND DATE_FORMAT(b.create_date, '%Y-%m-%d') = date_sub(DATE_FORMAT(a.create_date, '%Y-%m-%d'), INTERVAL 1 DAY)
+		ORDER BY b.create_date DESC LIMIT 1
+	) AS fans_num
+	, a.like_num - (
+		SELECT b.like_num
+		FROM spider_data b
+		WHERE b.plantform = a.plantform
+			AND DATE_FORMAT(b.create_date, '%Y-%m-%d') = date_sub(DATE_FORMAT(a.create_date, '%Y-%m-%d'), INTERVAL 1 DAY)
+		ORDER BY b.create_date DESC LIMIT 1
+	) AS like_num, (
+		SELECT b.rank_num
+		FROM spider_data b
+		WHERE b.plantform = a.plantform
+			AND DATE_FORMAT(b.create_date, '%Y-%m-%d') = date_sub(DATE_FORMAT(a.create_date, '%Y-%m-%d'), INTERVAL 1 DAY)
+		ORDER BY b.create_date DESC LIMIT 1
+	) - a.rank_num AS rank_num
+	, a.create_date
+FROM (SELECT * FROM spider_data ORDER BY create_date DESC LIMIT 1000000000000000) a
+WHERE a.plantform = 'cnblog'
+GROUP BY DATE_FORMAT(a.create_date, '%Y-%m-%d');
