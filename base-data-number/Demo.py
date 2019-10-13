52aa7d2bb771fadf3c8b2a2bca3e2049265727e3
inwsy <inwsy@hotmail.com>
2019-10-14 02:20:51 +0800

    新增 基础数据类型-number 代码示例

---
+++ base-data-number/Demo.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+
+print(type(123))
+
+print(type(123.0))
+
+print(type('123'))
+
+print(type("123"))
+
+print(isinstance(123, int))
+
+print(isinstance(123.0, float))
+
+print(isinstance('123', str))
+
+print(10**1000)
+
+print(0b10101010101)
+
+print(0o12345670)
+
+print(0xdb273dc)
+
+print(0xDB273DC)
+
+print(1/2)
+
+print(1/3)
+
+print(1/6)
+
+print(1+1j)
+
+print(type(1+1j))
+
+print((2.46+1.37j).real)
+
+print((2.46+1.37j).imag)
+
+print(123 == 123.0)
+
+print(123 == '123')
