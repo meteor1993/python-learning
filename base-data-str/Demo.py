48ff5cb4fd42b24c366ee29f94cb199929d44d99
inwsy <inwsy@hotmail.com>
2019-10-15 23:43:29 +0800

    更新 base-data-str 基础数据类型字符串示例代码

--- base-data-str/Demo.py
+++ base-data-str/Demo.py
@@ -1,3 +1,53 @@
 print('www.geekdigging.com')
 
 print("www.geekdigging.com")
+
+print('小明说："吃了吗？"')
+
+print("小明说：\"吃了吗？\"")
+
+print('''
+这是一个多行字符串的实例
+多行字符串可以使用制表符
+TAB ( \t )。
+也可以使用换行符 [ \n ]。
+''')
+
+print('I \nlike Python!')
+print("")
+print('I\tlike\tPython!')
+
+print("横向制表符：\\t")
+
+print(r"横向制表符：\t")
+
+print(len('I like Python!'))
+
+print(len('I like 小明!'))
+
+print(len(''))
+
+print(len(' '))
+
+print(len("\n"))
+
+a = "Hello"
+b = "Python"
+
+print("a + b 输出结果：", a + " " + b)
+
+print("a * 2 输出结果：", a * 2)
+
+print("a[1] 输出结果：", a[1])
+
+print("a[1:4] 输出结果：", a[1:4])
+
+if "H" in a:
+    print("H 在变量 a 中")
+else:
+    print("H 不在变量 a 中")
+
+if "M" not in a:
+    print("M 不在变量 a 中")
+else:
+    print("M 在变量 a 中")
