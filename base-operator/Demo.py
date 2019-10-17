8526adee98123fa17d03c674f7569735ca8f96c7
inwsy <inwsy@hotmail.com>
2019-10-18 00:47:40 +0800

    新增 python 算数运算符示例

---
+++ base-operator/Demo.py
@@ -0,0 +1,41 @@
+a = 5
+b = 10
+
+c = a + b
+print("1. c 的值为：", c)
+
+c = a - b
+print("2. c 的值为：", c)
+
+c = a * b
+print("3. c 的值为：", c)
+
+c = a / b
+print("4. c 的值为：", c)
+
+c = a % b
+print("5. c 的值为：", c)
+
+c = a ** b
+print("6. c 的值为：", c)
+
+# 改变 a 和 b 的值
+
+a = 10
+b = 5
+
+c = a // b
+print("7. c 的值为：", c)
+
+a = 1
+b = 1.5
+c = a + b
+print("8. c 的值为：", c, "，c 的类型为：", type(c))
+
+d = True
+e = c + d
+print("9. e 的值为：", e, "，e 的类型为：", type(e))
+
+f = 2 + 4j
+g = e + f
+print("10. g 的值为：", g, "，g 的类型为：", type(g))
