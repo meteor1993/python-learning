f52b4636cd99c8af4fbb343c8f846449217dba13
inwsy <inwsy@hotmail.com>
2019-10-23 00:04:11 +0800

    更新 基础流程控制（下）示例代码

--- base-process/Demo1.py
+++ base-process/Demo1.py
@@ -15,3 +15,29 @@ print("我不快乐了")
 
 for index in "Python":
     print(index)
+
+for index in range(5):
+    print(index)
+
+for index in range(0, 10, 3):
+    print(index)
+
+happy = 0
+
+while happy < 10:
+    happy += 1
+    if happy == 5:
+        break
+    print("学习使我快乐，快乐 + 1，当前快乐值为：", happy)
+
+print("还是开黑更快乐一些~~~")
+
+happy = 0
+
+while happy < 10:
+    happy += 1
+    if happy == 5:
+        continue
+    print("学习使我快乐，快乐 + 1，当前快乐值为：", happy)
+
+print("还是学习会更快乐~~~")
