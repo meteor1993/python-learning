802b7c4402390f3601fdea20cecef86caab435db
inwsy <inwsy@hotmail.com>
2019-11-01 00:11:24 +0800

    更新 小白学 Python（17）：基础数据类型（函数）（下）示例代码

--- base-data-def/Demo1.py
+++ base-data-def/Demo1.py
@@ -1,12 +1,29 @@
 a = 0
 
 def print_1():
-    a = 1
-    print('a =', a)
+    # a = 1
+    print('a1 =', a)
 
     def print_2():
         a = 2
-        print('a =', a)
+        print('a2 =', a)
 
+    print_2()
+
+print('a3 =', a)
 print_1()
-print('a =', a)
+
+add = lambda x,y: x + y
+
+print(add(1, 2))
+
+max_num = lambda x,y: x if x >= y else y
+
+print(max_num(5, 9))
+
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+print('10的阶乘为：', factorial(10))
