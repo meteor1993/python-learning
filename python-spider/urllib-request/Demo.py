d1375f8eb0710452eeebd46a468aebd840b05884
inwsy <inwsy@hotmail.com>
2019-11-28 17:09:46 +0800

    更新示例代码

--- python-spider/urllib-request/Demo.py
+++ python-spider/urllib-request/Demo.py
@@ -5,3 +5,25 @@ response = urllib.request.urlopen('https://www.geekdigging.com/')
 # print(response.read().decode('utf-8'))
 
 print(type(response))
+# 获取HTTP协议版本号(10 for HTTP/1.0, 11 for HTTP/1.1)
+print(response.version)
+
+# 获取响应码
+print(response.status)
+print(response.getcode())
+
+# 获取响应描述字符串
+print(response.reason)
+
+# 获取实际请求的页面url(防止重定向用)
+print(response.geturl())
+
+# 获取特定响应头信息
+print(response.getheader(name="Content-Type"))
+# 获取响应头信息,返回二元元组列表
+print(response.getheaders())
+# 获取响应头信息,返回字符串
+print(response.info())
+
+# 读取响应体
+print(response.readline().decode('utf-8'))
