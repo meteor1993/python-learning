5fc9c5966c44156163b741d24492ac0829aa63de
inwsy <inwsy@hotmail.com>
2019-12-10 23:33:49 +0800

    更新 小白学 Python 爬虫（19）：Xpath 基操 示例代码

--- python-spider/xpath-demo/xpath_demo.py
+++ python-spider/xpath-demo/xpath_demo.py
@@ -13,3 +13,18 @@ print(result_1)
 
 result_2 = html.xpath('//meta')
 print(result_2)
+
+result_3 = html.xpath('//main/article')
+print(result_3)
+
+result_4 = html.xpath('//main//div')
+print(result_4)
+
+result_5 = html.xpath('//img[@alt="小白学 Python 爬虫（16）：urllib 实战之爬取妹子图"]/../@href')
+print(result_5)
+
+result_6 = html.xpath('//img[@alt="小白学 Python 爬虫（16）：urllib 实战之爬取妹子图"]/parent::*/@href')
+print(result_6)
+
+result_7 = html.xpath('//section/div[@class="container"]')
+print(result_7)
