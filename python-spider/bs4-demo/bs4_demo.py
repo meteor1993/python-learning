ab0f2afe71cc967461909009424d59073ec45fde
inwsy <inwsy@hotmail.com>
2019-12-12 23:22:37 +0800

    更新 小白学 Python 爬虫（21）：解析库 Beautiful Soup（上）示例代码

--- python-spider/bs4-demo/bs4_demo.py
+++ python-spider/bs4-demo/bs4_demo.py
@@ -2,6 +2,41 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get('https://www.geekdigging.com/')
-html_str = response.content.decode('UTF-8')
-soup = BeautifulSoup(html_str, 'html.parser')
-print(soup)
+soup = BeautifulSoup(response.content, "html5lib")
+# 打印内容过长，先注释
+# print(soup.prettify())
+
+print(soup.title)
+print(type(soup.title))
+print(soup.title.string)
+print(soup.a)
+
+tag = soup.section
+print(tag.name)
+
+print(tag['class'])
+print(tag.attrs)
+
+print(soup.title.string)
+
+print(soup.a.img)
+print(type(soup.a.img))
+print(soup.a.img.attrs)
+
+# 获取子节点
+print(soup.article.contents)
+
+for i, child in enumerate(soup.article.children):
+    print(i, child)
+
+# 获取所有子孙节点
+for i, child in enumerate(soup.article.descendants):
+    print(i, child)
+# 父节点
+print(soup.title.parent)
+
+# 兄弟节点
+print('next_sibling：', soup.title.next_sibling)
+print('previous_sibling：', soup.title.previous_sibling)
+print('next_siblings：', soup.title.next_siblings)
+print('previous_siblings：', soup.title.previous_siblings)
