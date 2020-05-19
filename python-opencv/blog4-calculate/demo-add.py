66fa60781881d55e816f1d351510cb09fbd4d1fe
inwsy <inwsy@hotmail.com>
2020-05-19 17:02:57 +0800

    新增 「Python 图像处理 OpenCV （4）：图像算数运算以及修改颜色空间」 示例代码

---
+++ python-opencv/blog4-calculate/demo-add.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+# 读取图像
+img = cv.imread("maliao.jpg", cv.IMREAD_UNCHANGED)
+
+test = img
+
+result1 = img + test
+
+result2 = cv.add(img, test)
+
+# 显示图像
+cv.imshow("img", img)
+cv.imshow("result1", result1)
+cv.imshow("result2", result2)
+
+# 等待显示
+cv.waitKey()
+cv.destroyAllWindows()
---
+++ python-opencv/blog4-calculate/demo-addWeighted.py
@@ -0,0 +1,6 @@
+import cv2 as cv
+
+# 读取图像
+img1 = cv.imread("maliao.jpg", cv.IMREAD_UNCHANGED)
+img2 = cv.imread("maliao1.jpg", cv.IMREAD_UNCHANGED)
+
